37a7c44b52202a44ec53e36559503a42671f6751
Abdul Qayyum <abdulqayyum3810@gmail.com>
2022-07-20 20:23:41 -0700

    Asp assignment 2

---
+++ Task_1_student data/Student_data.aspx
@@ -0,0 +1,60 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Student_data.aspx.cs" Inherits="Task1_Student_data.Student_data" %>
+
+<%@ Register Assembly="AjaxControlToolkit" Namespace="AjaxControlToolkit" TagPrefix="ajaxToolkit" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <title></title>
+    <script type="text/javascript">
+
+
+
+            $("#StudentListDropDown").on("change", function () {
+                var studentName = $("#StudentListDropDown").val();
+                console.log(studentName);
+
+                $.ajax({
+
+                    url: "Student_data.aspx/GetStudentData",
+                    type: "GET",
+                    contentType: "application/json; charset=utf-8",
+                    data= { "name": studentName },
+                    dataType: "json",
+                    success: function (data) {
+
+                        /*$("#student-data").text(data.d.Name)*/;
+                        console.log(data.d)
+
+                    },
+                    error: function (data) {
+                        alert('error');
+                    }
+
+                });
+
+            });
+
+
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:DropDownList ID="StudentListDropDown"  runat="server" ></asp:DropDownList>
+            <asp:ScriptManager runat="server"></asp:ScriptManager>
+            <button id="btn" >change</button>
+            <h2 id="student-data">
+
+
+            </h2>
+            
+        </div>
+    </form>
+
+
+</body>
+
+</html>
---
+++ Task_1_student data/Student_data.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Services;
+
+namespace Task1_Student_data
+{
+    public class Student {
+        public int RollNo { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+    }
+    public partial class Student_data : System.Web.UI.Page
+    {
+        protected static List<Student> studentList ;
+
+
+        protected void Page_Load(object sender, EventArgs e)
+
+        {
+            var s1 = new Student() { RollNo = 1, Name = "Abdullah", Department = "Mechatronics" };
+            var s2 = new Student() { RollNo = 2, Name = "Ahmad", Department = "Electrical" };
+            var s3 = new Student() { RollNo = 3, Name = "Fuqan", Department = "Electrical" };
+            var s4 = new Student() { RollNo = 4, Name = "Khuram", Department = "Computer Science" };
+            var s5 = new Student() { RollNo = 5, Name = "Abdul Qayyum", Department = "Mechatronics" };
+            var s6 = new Student() { RollNo = 6, Name = "Shahbaz", Department = "Civil" };
+            var s7 = new Student() { RollNo = 7, Name = "Daim", Department = "IT" };
+            var s8 = new Student() { RollNo = 8, Name = "Huzaifa", Department = "Automation" };
+
+            studentList = new List<Student>() { s1, s2, s3, s4, s5, s6, s7, s8 };
+            StudentListDropDown.DataSource = studentList;
+            StudentListDropDown.DataTextField = "Name";
+            StudentListDropDown.DataValueField = "Name";
+            StudentListDropDown.DataBind();
+        }
+
+        [WebMethod]
+        public static string GetStudentData(string name)
+        {
+            foreach(var student in studentList)
+            {
+                if (student.Name == name)
+                {
+                    return "you got it";
+                }
+            }
+            return "You di not got it";
+        }
+    }
+}
